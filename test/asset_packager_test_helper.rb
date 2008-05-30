ae509663203123a89d6a8573712d62088751a505
Gavin Stark <g.stark@realdigitalmedia.com>
2008-05-29 17:47:05 -0700

    Improving regex to match the base files to support a revision # with alphabetic chars.  Added ability to read revision from a REVISION file in RAILS_ROOT.  Fixed tests so they run independent of a rails project.

---
+++ test/asset_packager_test_helper.rb
@@ -0,0 +1,57 @@
+require 'test/unit'
+require 'rubygems'
+require 'mocha'
+require 'yaml'
+
+RAILS_ROOT = File.dirname(__FILE__) + "/fake_rails_root"
+
+require 'action_controller'
+require 'action_controller/test_process'
+
+require 'synthesis/asset_package'
+require 'synthesis/asset_package_helper'
+ActionView::Base.send :include, Synthesis::AssetPackageHelper
+
+ActionController::Base.logger = nil
+ActionController::Base.ignore_missing_templates = false
+ActionController::Routing::Routes.reload rescue nil
+
+$asset_packages_yml = YAML.load_file( RAILS_ROOT + "/config/asset_packages.yml" )
+$asset_base_path    = RAILS_ROOT + "/public"
+
+class AssetPackageHelperTest < Test::Unit::TestCase
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::AssetTagHelper
+  include Synthesis::AssetPackageHelper
+
+  def setup
+    Synthesis::AssetPackage.any_instance.stubs(:log)
+    self.stubs(:should_merge?).returns(true)
+
+    @controller = Class.new do
+      def request
+        @request ||= ActionController::TestRequest.new
+      end
+    end.new
+  end
+
+  def test_nothing
+    true
+  end
+
+  def build_packages_once
+    unless @packages_built
+      Synthesis::AssetPackage.build_all
+      @packages_built = true
+    end
+  end
+  
+  def build_js_expected_string(*sources)
+    sources.map {|s| %(<script src="/javascripts/#{s}.js" type="text/javascript"></script>) }.join("\n")
+  end
+    
+  def build_css_expected_string(*sources)
+    sources.map {|s| %(<link href="/stylesheets/#{s}.css" rel="Stylesheet" type="text/css" media="screen" />) }.join("\n")
+  end
+
+end
